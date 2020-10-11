4072d3c6b89784303a62e371e411f1ee2cdeb91c
Wkhg <72546456+Wkhg@users.noreply.github.com>
2020-10-11 14:46:18 +0800

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,2 +1,6 @@
-# Display the output
-print("New Python file")
+import pandas as pd
+import numpy as np
+import seaborn as sns                       #visualisation
+import matplotlib.pyplot as plt             #visualisation
+%matplotlib inline     
+sns.set(color_codes=True)
